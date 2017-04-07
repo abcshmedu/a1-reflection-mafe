dc2757f5090c2c257f0838190ae19dd798345530
Felix Maurer <felix.m@online.de>
2017-04-07 13:47:39 +0200

    Finish test for output lines

--- src/test/java/edu/hm/RendererOutputTest.java
+++ src/test/java/edu/hm/RendererOutputTest.java
@@ -1,19 +1,34 @@
 package edu.hm;
 
+import java.util.Arrays;
 import java.util.Collection;
 
+import edu.hm.renderer.Renderer;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
 
 @RunWith(Parameterized.class)
 public class RendererOutputTest {
     
     @Parameters
     public static Collection<Object[]> data() {
-        
-        return null;
+        SomeClass object = new SomeClass(5);
+        return Arrays.asList(new Object[][] {
+                {object, "Instance of edu.hm.SomeClass:", true},
+                {object, "foo (Type int): 5", true},
+                {object, "array (Type int[]): [1, 2, 3, ]", true},
+                {object, "date (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970", true},
+                {object, "getArray() (ReturnType int[]): [1, 2, 3, ]", true},
+                {object, "getFoo() (ReturnType int): 5", true},
+                {object, "getDate()", false},
+        });
     }
     
     @Parameter
@@ -23,10 +38,15 @@ public class RendererOutputTest {
     public String string;
     
     @Parameter(2)
-    public boolean bool;
-    
+    public boolean shouldContain;
+
+    @Test
     public void test() {
-        
+        if (shouldContain) {
+            assertThat(new Renderer(obj).render(), containsString(string));
+        } else {
+            assertThat(new Renderer(obj).render(), not(containsString(string)));
+        }
     }
 
 }
